dcac38ff115fe4bb6fb04b84afa33e9bdbbb99e4
Geert Bergman <geert@scrivo.nl>
2014-07-11 03:17:50 +0200

    Incorporated new styles into the test case for 8.1

--- test/test.php
+++ test/test.php
@@ -79,8 +79,7 @@ $testResult .= "</p><p>Highlighting took ".
   <title>highlight.js test</title>
   <meta charset="utf-8">
 
-  <link rel="stylesheet" title="Default" 
-    href="../styles/default.css">
+  <link rel="stylesheet" title="Default" href="../styles/default.css">
   <link rel="alternate stylesheet" title="Dark"
     href="../styles/dark.css">
   <link rel="alternate stylesheet" title="FAR"
@@ -143,8 +142,43 @@ $testResult .= "</p><p>Highlighting took ".
     href="../styles/mono-blue.css">
   <link rel="alternate stylesheet" title="Foundation"
     href="../styles/foundation.css">
-
+  <link rel="alternate stylesheet" title="Atelier Dun - Dark"
+    href="../styles/atelier-dune.dark.css">
+  <link rel="alternate stylesheet" title="Atelier Dun - Light"
+    href="../styles/atelier-dune.light.css">
+  <link rel="alternate stylesheet" title="Atelier Forest - Dark"
+    href="../styles/atelier-forest.dark.css">
+  <link rel="alternate stylesheet" title="Atelier Forest - Light"
+    href="../styles/atelier-forest.light.css">
+  <link rel="alternate stylesheet" title="Atelier Heath - Dark"
+    href="../styles/atelier-heath.dark.css">
+  <link rel="alternate stylesheet" title="Atelier Heath - Light"
+    href="../styles/atelier-heath.light.css">
+  <link rel="alternate stylesheet" title="Atelier Lakeside - Dark"
+    href="../styles/atelier-lakeside.dark.css">
+  <link rel="alternate stylesheet" title="Atelier Lakeside - Light"
+    href="../styles/atelier-lakeside.light.css">
+  <link rel="alternate stylesheet" title="Atelier Seaside - Dark"
+    href="../styles/atelier-seaside.dark.css">
+  <link rel="alternate stylesheet" title="Atelier Seaside - Light"
+    href="../styles/atelier-seaside.light.css">
+  <link rel="alternate stylesheet" title="Paraíso - Dark"
+    href="../styles/paraiso.dark.css">
+  <link rel="alternate stylesheet" title="Paraíso - Light"
+    href="../styles/paraiso.light.css">
+  <link rel="alternate stylesheet" title="Colorbrewer"
+    href="../styles/color-brewer.css">
+  <link rel="alternate stylesheet" title="Codepen.io Embed"
+    href="../styles/codepen-embed.css">
+  <link rel="alternate stylesheet" title="Kimbie - Dark"
+    href="../styles/kimbie.dark.css">
+  <link rel="alternate stylesheet" title="Kimbie - Light"
+    href="../styles/kimbie.light.css">
+  <link rel="alternate stylesheet" title="Hybrid"
+    href="../styles/hybrid.css">
+  
   <style>
+    /* Base styles */
     body {
       font: small Arial, sans-serif;
     }
@@ -154,7 +188,9 @@ $testResult .= "</p><p>Highlighting took ".
       margin-bottom: 0.5em;
     }
     table {
-      width: 100%; padding: 0; border-collapse: collapse;
+      width: 100%;
+      padding: 0;
+      border-collapse: collapse;
     }
     th {
       width: 12em;
@@ -168,22 +204,42 @@ $testResult .= "</p><p>Highlighting took ".
       text-align: left;
     }
     pre {
-      margin: 0; font-size: medium;
+      margin: 0;
+      font-size: medium;
     }
-    #switch {
-      overflow: auto; width: 67em;
+    .hljs-debug {
+      color: red;
+    }
+    /* Style switcher */
+    ul#switch {
+      width: 66em;
+      -webkit-column-width: 15em;
+      -webkit-column-gap: 2em;
+      -moz-column-width: 15em;
+      -moz-column-gap: 2em;
+      -o-column-width: 15em;
+      -o-column-gap: 2em;
+      column-width: 15em;
+      column-gap: 2em;
       list-style: none;
-      padding: 0; margin: 0;
+      overflow: auto;
+      padding: 0;
+      margin: 0;
     }
-    #switch li {
-      float: left; width: 12em;
-      padding: 0.1em; margin: 0.1em 1em 0.1em 0;
+    ul#switch li {
+      -webkit-column-break-inside: avoid;
+      -moz-column-break-inside: avoid;
+      -o-column-break-inside: avoid;
+      column-break-inside: avoid;
+      padding: 0.1em;
+      margin: 0.1em 1em 0.1em 0;
       background: #EEE;
       cursor: pointer;
     }
-    #switch li.current {
+    ul#switch li.current {
       background: #CCC;
     }
+    /* Tests */
     .test {
       color: #888;
       font-weight: normal;
@@ -195,7 +251,7 @@ $testResult .= "</p><p>Highlighting took ".
     .passed {
       color: green;
     }
-    .failed {
+    .failed, .failed a {
       color: red;
     }
     .code {
@@ -204,69 +260,76 @@ $testResult .= "</p><p>Highlighting took ".
     .code .hljs-keyword {
       font-weight: bold;
     }
+    /* Export form */
+    #export_from, #export_to {
+      width: 98%;
+    }
+    address {
+      margin-top: 4em;
+    }
   </style>
+
   <script>
   // Stylesheet switcher © Vladimir Epifanov <voldmar@voldmar.ru>
-    (function(container_id) {
-        if (window.addEventListener) {
-            var attach = function(el, ev, handler) {
-                el.addEventListener(ev, handler, false);
-            }
-        } else if (window.attachEvent) {
-            var attach = function(el, ev, handler) {
-                el.attachEvent('on' + ev, handler);
-            }
-        } else {
-            var attach = function(el, ev, handler) {
-                ev['on' + ev] = handler;
-            }
-        }
+  (function(container_id) {
+      if (window.addEventListener) {
+          var attach = function(el, ev, handler) {
+              el.addEventListener(ev, handler, false);
+          }
+      } else if (window.attachEvent) {
+          var attach = function(el, ev, handler) {
+              el.attachEvent('on' + ev, handler);
+          }
+      } else {
+          var attach = function(el, ev, handler) {
+              ev['on' + ev] = handler;
+          }
+      }
 
-        attach(window, 'load', function() {
-            var current = null;
+      attach(window, 'load', function() {
+          var current = null;
 
-            var info = {};
-            var links = document.getElementsByTagName('link');
-            var ul = document.createElement('ul')
+          var info = {};
+          var links = document.getElementsByTagName('link');
+          var ul = document.createElement('ul');
 
-            for (var i = 0; (link = links[i]); i++) {
-                if (link.getAttribute('rel').indexOf('style') != -1
-                        && link.title) {
+          for (var i = 0; (link = links[i]); i++) {
+              if ((link.getAttribute('rel').indexOf('style') != -1) && link.title) {
+                  var title = link.title;
 
-                    var title = link.title;
+                  info[title] = {
+                      'link': link,
+                      'li': document.createElement('li')
+                  };
 
-                    info[title] = {
-                        'link': link,
-                        'li': document.createElement('li')
-                    }
+                  ul.appendChild(info[title].li);
+                  info[title].li.title = title;
 
-                    ul.appendChild(info[title].li)
-                    info[title].li.title = title;
+                  info[title].link.disabled = true;
 
-                    info[title].link.disabled = true;
+                  info[title].li.appendChild(document.createTextNode(title));
 
-                    info[title].li.appendChild(document.createTextNode(title));
-                    attach(info[title].li, 'click', (function (el) {
-                        return function() {
-                            current.li.className = '';
-                            current.link.disabled = true;
-                            current = el;
-                            current.li.className = 'current';
-                            current.link.disabled = false;
-                        }})(info[title]));
-                }
-            }
+                  attach(info[title].li, 'click', (function (el) {
+                      return function() {
+                          current.li.className = '';
+                          current.link.disabled = true;
+                          current = el;
+                          current.li.className = 'current';
+                          current.link.disabled = false;
+                      }
+                  })(info[title]));
+              }
+          }
 
-            current = info['Default']
-            current.li.className = 'current';
-            current.link.disabled = false;
+          current = info['Default'];
+          current.li.className = 'current';
+          current.link.disabled = false;
 
-            ul.id = 'switch';
-            container = document.getElementById(container_id);
-            container.appendChild(ul);
-        });
-
-    })('styleswitcher');
+          ul.id = 'switch';
+          container = document.getElementById(container_id);
+          container.appendChild(ul);
+      });
+  })('styleswitcher');
   </script>
 <body>
 
