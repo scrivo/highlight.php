b184006a44783bc142a00ea8d49d241f707ec90c
Vladimir Jimenez <allejo@me.com>
2017-12-24 15:01:16 -0800

    Convert E_ERRORS into exceptions in unit tests

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,3 +1,12 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+
+// Throw an exception when E_ERRORS occur to better debug the problem
+function throwExceptionOnError($severity, $message, $file, $line) {
+    $errMessage = sprintf('Language error ("%s") thrown from %s:%s', $message, $file, $line);
+
+    throw new \Exception($errMessage, $severity);
+}
+
+set_error_handler('throwExceptionOnError');
