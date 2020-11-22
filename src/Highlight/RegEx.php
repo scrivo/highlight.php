7c4c207322ebfad6ed4f6cc96c2f938b3c722287
Vladimir Jimenez <allejo@me.com>
2020-11-21 21:52:44 -0800

    Fix atrocious performance in RegEx class
    Forward port e98ea8f585a844ced8d6d3617add468965ebb96a
    
    Fixes #78

--- src/Highlight/RegEx.php
+++ src/Highlight/RegEx.php
@@ -76,13 +76,13 @@ final class RegEx
     {
         $index = null;
         $results = array();
-        preg_match_all($this->source, $str, $results, PREG_SET_ORDER | PREG_OFFSET_CAPTURE, $this->lastIndex);
+        preg_match($this->source, $str, $results, PREG_OFFSET_CAPTURE, $this->lastIndex);
 
         if ($results === null || count($results) === 0) {
             return null;
         }
 
-        foreach ($results[0] as &$result) {
+        foreach ($results as &$result) {
             if ($result[1] !== -1) {
                 // Only save the index if it hasn't been set yet
                 if ($index === null) {
@@ -95,7 +95,8 @@ final class RegEx
             }
         }
 
-        $results = $results[0];
+        unset($result);
+
         $this->lastIndex += mb_strlen($results[0]) + ($index - $this->lastIndex);
 
         $matches = new RegExMatch($results);
